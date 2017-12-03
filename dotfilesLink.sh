27c82457726ef6d559f9c10c6bdcb98db9490051
shitaro <apprentice.physicist.shitaro@gmail.com>
2017-12-03 18:25:41 +0900

    Add setting of urxvt

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,5 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.xkb ~/.xkb
+ln -sf ~/dotfiles/.Xresources ~/.Xresources
+ln -sf ~/dotfiles/.Xresources.d ~/.Xresources.d
