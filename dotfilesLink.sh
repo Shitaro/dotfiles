9cec6a723201ea2525e25face877d3220911ba2a
shitaro <apprentice.physicist.shitaro@gmail.com>
2017-12-03 19:37:45 +0900

    Add config of i3wm

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,3 +6,5 @@ ln -sf ~/dotfiles/.xkb ~/.xkb
 ln -sf ~/dotfiles/.Xresources ~/.Xresources
 ln -sf ~/dotfiles/.Xresources.d ~/.Xresources.d
 ln -sf ~/dotfiles/.colorrc ~/.colorrc
+ln -sf ~/dotfiles/.config/i3 ~/.config/i3
+ln -sf ~/dotfiles/.config/i3status ~/.config/i3status
