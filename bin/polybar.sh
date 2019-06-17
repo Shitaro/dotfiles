eec4cc2cc82a38bd87ccd6ab4bb9b95692646c44
shitaro <apprentice.physicist.shitaro@gmail.com>
2019-06-17 23:43:55 +0900

    Replace eth_interface with env var

--- bin/polybar.sh
+++ bin/polybar.sh
@@ -9,6 +9,9 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Get connected monitors
 monitor_list=$(xrandr -q | grep " connected" | cut -d ' ' -f 1)
 
+export ETH_INTERFACE
+ETH_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+
 # Launch bars
 for monitor in ${monitor_list}; do
     polybar --reload ${monitor} &
